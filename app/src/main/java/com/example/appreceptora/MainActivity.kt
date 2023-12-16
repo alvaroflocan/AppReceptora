5c5ea45c1a1dbcf533a0bba303f2428097c40c73
alvaroflo <alvaro.flocan@educa.jcyl.es>
2023-12-16 11:25:38 +0100

    Refact

--- app/src/main/java/com/example/appreceptora/MainActivity.kt
+++ app/src/main/java/com/example/appreceptora/MainActivity.kt
@@ -1,85 +1,33 @@
 package com.example.appreceptora
-
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.net.wifi.WifiManager
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.NotificationCompat
-private val ID_CANAL = "app_receptora_01"
-private val idNotificacion = 102
-/*private val conexionReceiver = object : BroadcastReceiver() {
-    /**
-     * Necesita el contexto y el intent para pasar la información
-     */
-    override fun onReceive(context: Context, intent: Intent) {
-
-        val mensajeapp1 = intent?.extras?.getString("mensaje") ?: "Sin mensaje"
-        // Intent para reiniciar la aplicación cuando se haga clic en la notificación
-
-        val administradorNotificaciones = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // Creo una notificación con el método Builder, pasando el contexto y aseguro que este no
-        // será nulo, le paso además el ID del canl
-        // Le paso los parámetros para crearla.
-        val notificacion = NotificationCompat.Builder(context!!, ID_CANAL)
-            .setContentTitle("Alarma Activada")
-            .setContentText("Tu alarma programada se ha activado.")
-            .setSmallIcon(R.drawable.ic_notificacion)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setAutoCancel(true)  // La notificación se cierra automáticamente al hacer clic en ella
-            .build()
-
-        //Muestra la notificación
-        administradorNotificaciones.notify(idNotificacion, notificacion)
-    }
-
-}*/
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mensajeapp1 = intent?.extras?.getString(Intent.EXTRA_TEXT) ?: ""
-        if(mensajeapp1 != "") {
-            // Crear el canal de notificación
-            createNotificationChannel()
-            val administradorNotificaciones =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-            val notificacion = NotificationCompat.Builder(this, ID_CANAL)
-                .setContentTitle("Nuevo mensaje")
-                .setContentText(mensajeapp1)
-                .setSmallIcon(R.drawable.ic_notificacion)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true)  // La notificación se cierra automáticamente al hacer clic en ella
-                .build()
-
-            administradorNotificaciones.notify(idNotificacion, notificacion)
+        // Comprobar permisos
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // Si no se tienen los permisos, solicitarlos al usuario
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
         }
 
     }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(idNotificacion.toString(),ID_CANAL, importance)
-
-            // Registrar el canal en el NotificationManager
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-
-
 }
--- app/src/main/java/com/example/appreceptora/NotificacionReceiver.kt
+++ app/src/main/java/com/example/appreceptora/NotificacionReceiver.kt
@@ -1,39 +1,61 @@
 package com.example.appreceptora
-
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
-class NotificacionReceiver : BroadcastReceiver() {
-    // Se establecen los valores del ID del canl y de la notifivación
+class NotificationReceiver : BroadcastReceiver() {
 
-    private val ID_CANAL = "app_receptora_01"
-    private val idNotificacion = 102
-    private lateinit var notificationManager: NotificationManager
+    private val CHANNEL_ID = "channelId"
+    private val NOTIFICATION_ID = 1
 
-    /**
-     * Método que sobreescribe la función onRecive
-     */
     override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent?.getStringExtra("message")
-        notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        showNotification(message ?: "Mensaje vacío", context)
+        // Manejar la recepción del mensaje
+
+        val mensaje = intent?.getStringExtra("mensaje")
+        Log.d("AppReceptora", mensaje.toString())
+        showNotification(context, mensaje ?: "Mensaje vacío")
     }
 
-    private fun showNotification(message: String, context: Context?) {
+    @SuppressLint("MissingPermission","Solicitado en MainActivity")
+    private fun showNotification(context: Context?, message: String) {
+        // Crear un canal de notificación (necesario para Android 8.0 y versiones superiores)
+        createNotificationChannel(context)
 
-        val notification = NotificationCompat.Builder(context!!, ID_CANAL)
+        // Crear la notificación
+        val notification = NotificationCompat.Builder(context!!, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notificacion)
             .setContentTitle("Nuevo Mensaje")
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
             .build()
 
         // Mostrar la notificación
-        notificationManager.notify(idNotificacion, notification)
+        val notificationManager = NotificationManagerCompat.from(context!!)
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    private fun createNotificationChannel(context: Context?) {
+        // Crear el canal de notificación
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val name = "Canal de Notificación"
+            val descriptionText = "Canal para mostrar notificaciones"
+            val importance = NotificationManagerCompat.IMPORTANCE_MAX
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            // Registrar el canal en el NotificationManager
+            val notificationManager =
+                context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
+
